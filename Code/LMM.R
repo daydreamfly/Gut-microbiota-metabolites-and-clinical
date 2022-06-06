d0f082afa13130a0cae052a89d79a11008bafe86
Yanfan <fanyanqun204@163.com>
2022-06-06 16:24:48 +0800

    Update LMM.R

--- Code/LMM.R
+++ Code/LMM.R
@@ -1,15 +1,10 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
 library(lme4)
 library(sjstats)
 library(car)
 options(stringsAsFactors = F, warn = -1)
-
 data <- openxlsx::read.xlsx(paste("Data/","Data_for_LMM.xlsx", sep = ""))
 index <- colnames(data)
-
 result <- data.frame(p = numeric(),stringsAsFactors=FALSE)
-
 for(j in 3:ncol(data)){
   run <- paste("lmer(",index[j]," ~ 1 + time + (1|SampleID)",",data = data,","control =lmerControl(check.nobs.vs.nlev = 'ignore',
      check.nobs.vs.rankZ = 'ignore',check.nobs.vs.nRE='ignore'))",sep="")
