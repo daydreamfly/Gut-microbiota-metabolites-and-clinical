b91f4662a42f1774e116ae5d26a516aa2d80e012
Yanfan <fanyanqun204@163.com>
2022-06-06 16:25:08 +0800

    Update Mediation.R

--- Code/Mediation.R
+++ Code/Mediation.R
@@ -1,13 +1,9 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(mediation)
 library(openxlsx)
-rm(list = ls())
-
 data <- read.xlsx(paste("Data/","Data_for_mediation.xlsx", sep = ""))
 data <- data.frame(data[,c(1:2)], apply(data[,-c(1:2)], 2, function(x) x = x*1000/sum(x, na.rm = T)))
 data <- data[-which(rowSums(is.na(data)) > 0),] 
 mod.metabo <- lm(paste("Metabo"," ~ ","Bac", sep = ""), data = data)
 mod.indir<- lm(paste("Cli", "~", "Metabo", "+", "Bac", sep = ""), data = data)
-
 mod.med = mediate(mod.metabo, mod.indir, treat = "Bac", mediator = "Metabo", sims = 10000)
 summary(mod.med)
